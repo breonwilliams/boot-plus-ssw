78c7b6992fef243227f866698bdd0511d69491c2
breonw <breon_williams@unc.edu>
2018-11-25 14:02:38 -0500

    modify custom post type, taxonomies, and custom fields

--- assets/cpt-ui/post-types.php
+++ assets/cpt-ui/post-types.php
@@ -6,3 +6,41 @@
  * Time: 8:40 AM
  */
 
+function cptui_register_my_cpts_staff() {
+
+    /**
+     * Post Type: Staff.
+     */
+
+    $labels = array(
+        "name" => __( "Staff", "" ),
+        "singular_name" => __( "Staff", "" ),
+    );
+
+    $args = array(
+        "label" => __( "Staff", "" ),
+        "labels" => $labels,
+        "description" => "",
+        "public" => true,
+        "publicly_queryable" => true,
+        "show_ui" => true,
+        "delete_with_user" => false,
+        "show_in_rest" => true,
+        "rest_base" => "",
+        "rest_controller_class" => "WP_REST_Posts_Controller",
+        "has_archive" => true,
+        "show_in_menu" => true,
+        "show_in_nav_menus" => true,
+        "exclude_from_search" => false,
+        "capability_type" => "post",
+        "map_meta_cap" => true,
+        "hierarchical" => false,
+        "rewrite" => array( "slug" => "staff", "with_front" => true ),
+        "query_var" => true,
+        "supports" => array( "title", "editor", "thumbnail", "excerpt", "custom-fields", "page-attributes" ),
+    );
+
+    register_post_type( "staff", $args );
+}
+
+add_action( 'init', 'cptui_register_my_cpts_staff' );
--- assets/cpt-ui/taxonomies.php
+++ assets/cpt-ui/taxonomies.php
@@ -7,3 +7,34 @@
  */
 
 
+function cptui_register_my_taxes() {
+
+    /**
+     * Taxonomy: Staff Categories.
+     */
+
+    $labels = array(
+        "name" => __( "Staff Categories", "" ),
+        "singular_name" => __( "Staff Category", "" ),
+    );
+
+    $args = array(
+        "label" => __( "Staff Categories", "" ),
+        "labels" => $labels,
+        "public" => true,
+        "publicly_queryable" => true,
+		"hierarchical" => true,
+		"show_ui" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"query_var" => true,
+		"rewrite" => array( 'slug' => 'staff_category', 'with_front' => true, ),
+		"show_admin_column" => false,
+		"show_in_rest" => false,
+		"rest_base" => "staff_category",
+		"rest_controller_class" => "WP_REST_Terms_Controller",
+		"show_in_quick_edit" => false,
+		);
+	register_taxonomy( "staff_category", array( "staff" ), $args );
+}
+add_action( 'init', 'cptui_register_my_taxes' );
